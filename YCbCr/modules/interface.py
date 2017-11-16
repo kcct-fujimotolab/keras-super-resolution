6f8a9535ff0b4319688924bee6abaf7cbd305bec
yoidea <yoidea.cc@gmail.com>
2017-11-16 10:39:13 +0900

    Add YCbCr version

---
+++ YCbCr/modules/interface.py
@@ -0,0 +1,29 @@
+import numpy as np
+from PIL import Image
+
+
+def show(y_image, c_image):
+    """
+    画像データを表示
+    # 引数
+        image : Numpy array, 画像データ
+    """
+    y_image = y_image[0] * 255
+    c_image = c_image[0] * 255
+    image = np.concatenate((y_image, c_image))
+    image = image.astype(np.uint8)
+    image = Image.fromarray(image)
+    image.show()
+
+
+def get_input():
+    """
+    標準入力から文字列を取得
+    # 戻り値
+        value : String, 入力値
+    """
+    value = input('>> ')
+    value = value.rstrip()
+    if value == 'q':
+        exit()
+    return value
